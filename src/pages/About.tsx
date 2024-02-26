060a4dbbd1489fb559609bd0d5728993d3646bea
Brendon Butler <brendonaaron97@gmail.com>
2024-02-26 17:38:57 -0500

    Create pages, components, styles, and hooks and add assets to match original inspiration

---
+++ src/pages/About.tsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+function About() {
+    return (
+        <Fragment>
+            <p>
+                About me
+            </p>
+        </Fragment>
+    );
+}
+
+export default About;
---
+++ src/pages/Portfolio.tsx
@@ -0,0 +1,13 @@
+import { Fragment } from 'react';
+
+function Portfolio() {
+    return (
+        <Fragment>
+            <p>
+                Portfolio
+            </p>
+        </Fragment>
+    );
+}
+
+export default Portfolio;
